996b636a6c7ed71ad9b2cad39ae798591565799a
RaiBnod <binodrayee@gmail.com>
2020-07-13 23:32:15 +0545

    Add header's buttons

--- src/ScheduleCalendar.tsx
+++ src/ScheduleCalendar.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import TimezoneToggle from './TimezoneToggle';
-import { stylesFactory } from '@grafana/ui';
+import { stylesFactory, useTheme } from '@grafana/ui';
 import { css } from 'emotion';
-import { Chip } from '@material-ui/core';
+import { Avatar, Chip } from '@material-ui/core';
 
 export default function ScheduleCalendar() {
+  const theme = useTheme();
   const styles = getStyles();
+  const color = theme.isDark ? 'primary' : 'default';
 
   return (
     <TimezoneToggle timezone="UTC">
       {(toggleTimezone, timezone, timezoneName) => (
         <div className={styles.title}>
           <Chip
+            className={styles.item}
             variant="outlined"
             size="small"
             label={`Timezone: ${timezoneName || 'UTC'}`}
-            color="primary"
+            color={color}
             onClick={toggleTimezone}
+            clickable
+          />
+          <div className={styles.blankSpace} />
+          <Chip
+            className={styles.item}
+            variant="outlined"
+            size="small"
+            avatar={<Avatar>+</Avatar>}
+            label="Weekly Event"
+            color={color}
+            clickable
+          />
+          <Chip
+            className={styles.item}
+            variant="outlined"
+            size="small"
+            avatar={<Avatar>+</Avatar>}
+            label="Event"
+            color={color}
             clickable
           />
         </div>
@@ -29,6 +51,17 @@ const getStyles = stylesFactory(() => {
   return {
     title: css`
       height: 14px;
+      display: flex;
+      flex-wrap: wrap;
+    `,
+    item: css`
+      flex-grow: 0;
+      &:last-child {
+        margin-left: 4px;
+      }
+    `,
+    blankSpace: css`
+      flex-grow: 1;
     `,
   };
 });
